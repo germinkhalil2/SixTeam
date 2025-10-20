7061c86092995b4f66c457c77f2bcdc5d136ee25
Angel Williams <angel_williams363@outlook.com>
2025-10-20 19:01:12 -0400

    Created and connected mysql database, created virtual environment

--- WellFed/WellFed/settings.py
+++ WellFed/WellFed/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'WellFed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'WellFed_DB',
+        'USER': 'root',
+        'PASSWORD': 'Cocomac1!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
