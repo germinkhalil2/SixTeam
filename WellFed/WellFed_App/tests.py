11c5d4a33e5f7a45ec64503d05961621a0658a9a
Angel Williams <angel_williams363@outlook.com>
2025-10-21 01:20:57 -0400

    Created single meal creation test case (pass)

--- WellFed/WellFed_App/tests.py
+++ WellFed/WellFed_App/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Single_Meal
 
 # Create your tests here.
+class SingleMealTestCase(TestCase):
+    def setUp(self):
+        # Set up a test meal
+        Single_Meal.objects.create_meal(
+            meal_name="Pancakes",
+            meal_id="121",
+            user_id="121",
+            recipe_link="http://example.com/test-meal",
+            meal_description="A test meal for unit testing.",
+            meal_type_choice="Snack",
+            calories=600,
+            protein=40.0,
+            carbs=50.0,
+            fats=20.0
+        )
+
+    def test_meal_creation(self):
+        # Test if the meal was created successfully
+        meal = Single_Meal.objects.get(meal_id="121")
+        self.assertEqual(meal.meal_name, "Pancakes")
+        self.assertEqual(meal.meal_type_choice, "Snack")
+        self.assertEqual(meal.calories, 600)
+        self.assertEqual(meal.protein, 40.0)
+        self.assertEqual(meal.carbs, 50.0)
+        self.assertEqual(meal.fats, 20.0)
